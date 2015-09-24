7ee87307ff4c99839c7f633e734e84942d86b073
formalLogicGirl <swaha.miller@gmail.com>
2015-09-23 21:23:24 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    blogger : {
+	host : 'www.googleapis.com',
+	api_key : '******',
+	access_token : '******r',
+	blog_id_read_from : '8325623739778803023',
+	post_id : '38281100828609716',
+	blog_id_write_to : '8325623739778803023'
+  }
+};
